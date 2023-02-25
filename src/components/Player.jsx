c85f76a323cb6cd0c26e4cde39cc47c73d8c34b6
sharkz <mosenjemudogoz@gmail.com>
2023-02-25 20:57:18 +0300

    set up react-redux

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,10 +3,12 @@ import {MdCast,MdPlayCircleFilled,MdPauseCircle,MdSkipNext,MdSkipPrevious,MdShuf
 import {CgPlayList} from "react-icons/cg"
 import { IconContext } from 'react-icons'
 import axios from 'axios'
+import { useDispatch,useSelector } from 'react-redux'
 
 const Player = () => {
   const [token,setToken]=useState("");
-  const [song,setSong]=useState({});
+  const dispatch=useDispatch();
+  const {currentSong}=useSelector(state=>state.currentSong)
 
   useEffect(()=>{
     setToken(window.localStorage.getItem("token"));    
@@ -16,24 +18,26 @@ const Player = () => {
     if(token != ""){
       playerState();
     }
-  },[token])
+  },[token,dispatch])
 
   const playerState = async () => {
+    dispatch({"type":"GET_CURRENT_SOGN_REQUEST"})
     const {data} = await axios.get("https://api.spotify.com/v1/me/player", {
         headers: {
             Authorization: `Bearer ${token}`
         }   
     })
-    setSong(data)
+
+    dispatch({"type":"GET_CURRENT_SONG_SUCCESS","payload":data});
   }
 
   // console.log(song)
   return (
     <div className='player_container'>
-      {song ?(
+      {currentSong ?(
            <>
            <div>
-             <p>{song?.item?.name}{"\t "}Artist:{song?.item?.artists.map(artist=>artist.name)}</p>
+             <p>{currentSong?.item?.name}{"\t "}Artist:{currentSong?.item?.artists.map(artist=>artist.name)}</p>
            </div>
            <IconContext.Provider value={{ className: 'react-icons' ,size : "25"}}>
            <div className='player_icons_container'>
